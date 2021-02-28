8c3252d08197da352d5f1878e7fb2dccb00a9d03
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-02-28 14:52:13 +0200

    test

---
+++ class-01/js/app.js
@@ -0,0 +1,12 @@
+let userName = "Ahmad";
+let age = 23;
+const tax = 0.16;
+console.log("before changes: ", userName, age, tax);
+
+age = 50;
+console.log("after changes: ", userName, age, tax);
+
+alert('Hello Everyone');
+confirm('Do you want to continue');
+
+let userInput = prompt('enter your fav team: ');
